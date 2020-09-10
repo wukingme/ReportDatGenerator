b1c44a41ff99a92192c3e4da0f79fd19814c5776
wukingme <wukingme@163.com>
2020-09-10 22:05:18 +0800

    添加项目文件。

---
+++ ReportDatGenerator/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ReportDatGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDatFileName = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "生成的Dat文件名";
+            // 
+            // txtDatFileName
+            // 
+            this.txtDatFileName.Location = new System.Drawing.Point(133, 8);
+            this.txtDatFileName.Name = "txtDatFileName";
+            this.txtDatFileName.Size = new System.Drawing.Size(258, 21);
+            this.txtDatFileName.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "03Excel文件|*.xls|07Excel文件|*.xlsx";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "选择Excel文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(510, 80);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "生成Dat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(30, 158);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(510, 88);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "已生成Dat（可双击打开目录）";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 270);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDatFileName);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Dat文件生成";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDatFileName;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ ReportDatGenerator/MainForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReportDatGenerator
+{
+    public partial class MainForm : Form
+    {
+        private string strDocumnetSavePath = string.Empty;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.strGenerateDatFileName = $"{DateTime.Now:yyyyMMddHHmmss}.dat";
+            this.txtDatFileName.Text = this.strGenerateDatFileName;
+            this.strDocumnetSavePath = Path.Combine(Application.ExecutablePath, $"{DateTime.Now:yyyyMMdd}", this.strGenerateDatFileName);
+        }
+
+        /// <summary>
+        /// 当前选择的Excel文件
+        /// </summary>
+        private string strCurrentDataFile = string.Empty;
+
+        /// <summary>
+        /// 需要生成的Dat文件名
+        /// </summary>
+        private string strGenerateDatFileName = string.Empty;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (!string.IsNullOrEmpty(this.openFileDialog1.FileName))
+                    this.strCurrentDataFile = this.openFileDialog1.FileName;
+            }
+        }
+
+        /// <summary>
+        /// 生成Dat文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + strCurrentDataFile + ";" + "Extended Properties=Excel 8.0;";
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            OleDbDataAdapter myCommand = null;
+            string strExcel = string.Format("select * from [{0}$]", "Sheet1");
+            myCommand = new OleDbDataAdapter(strExcel, strConn);
+            myCommand.Fill(dt);
+            StringBuilder sbdDatData = new StringBuilder();
+            sbdDatData.AppendLine($"For M5|Adr     1|TO  {this.strCurrentDataFile}               |                      |                      |                      | ");
+            sbdDatData.AppendLine($"For M5|Adr     2|TO  Start-Line      aBFFB     4|                      |                      |                      | ");
+            sbdDatData.AppendLine($"For M5|Adr     3|KD1       J1                  4|                      |                      |Z         0.00000 m   | ");
+            if (dt.Rows.Count > 0)
+            {
+                int rowIndex = 4, dataIndex = 0;
+                double timeSpan = 15;
+                double rb1 = 0.00000f, rf2_1 = 0.00000f, rf2_2 = 0.00000f, rb1_2 = 0.00000f, z_Yzd = 0.00000f, z_Wzd = 0.00000f;
+                foreach (DataRow row in dt.Rows)
+                {
+                    dataIndex = dt.Rows.IndexOf(row);
+                    if (dataIndex == 0) continue;//跳过标题行
+                    rowIndex = rowIndex + dataIndex - 1;
+                    z_Wzd = Convert.ToDouble(row[3]);
+                    if (dataIndex % 2 == 0)
+                    {
+                        sbdDatData.AppendLine($"For M5|Adr     {dataIndex}|KD1       {row[0]}      {DateTime.Now.AddMilliseconds(timeSpan):HH:ss:iii}   4 |Rb        1.69016 m   |HD         10.253 m   |                      | ");
+                    }
+                }
+            }
+        }
+    }
+}
